12b58afd3a4798c1c284e14eccd892cb74cb8f93
TimurAgeenko <ageenko.timur.10@gmail.com>
2026-02-16 22:27:52 +0300

    Added Aeroplane class and tests for it

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 from unittest.mock import Mock, patch
 
-from src.models import APIHandler
+from src.models import APIHandler, Aeroplane
 
 
 def make_osm_response(bbox):
@@ -44,3 +44,37 @@ def test_get_aeroplane(mock_get):
     assert second_call[1]["params"]["lamax"] == bbox[1]
     assert second_call[1]["params"]["lomin"] == bbox[2]
     assert second_call[1]["params"]["lomax"] == bbox[3]
+
+
+def test_aeroplane_initialization(aeroplane):
+    assert aeroplane.callsign == "CALLSIGN"
+    assert aeroplane.country == "COUNTRY"
+    assert aeroplane.velocity == 1.0
+    assert aeroplane.altitude == 2.0
+
+
+def test_cast_to_object_list():
+    aeroplanes = [[None, "CALLSIGN", "COUNTRY", None, None, None, None, None, None, 1.0, None, None, None, 2.0]]
+    objects = Aeroplane.cast_to_object_list(aeroplanes)
+
+    assert len(objects) == 1
+    assert objects[0].callsign == "CALLSIGN"
+    assert objects[0].country == "COUNTRY"
+    assert objects[0].velocity == 1.0
+    assert objects[0].altitude == 2.0
+
+
+def test_get_top_altitude_aeroplanes(aeroplanes):
+    top_aeroplanes = Aeroplane.get_top_altitude_aeroplanes(aeroplanes, top_n=2)
+
+    assert len(top_aeroplanes) == 2
+    assert top_aeroplanes[0].callsign == "CALLSIGN4"
+    assert top_aeroplanes[1].callsign == "CALLSIGN3"
+
+
+def test_get_aeroplanes(aeroplanes):
+    sorted_aeroplanes = Aeroplane.get_aeroplanes(aeroplanes, "COUNTRY3")
+
+    assert len(sorted_aeroplanes) == 2
+    assert sorted_aeroplanes[0].callsign == "CALLSIGN3"
+    assert sorted_aeroplanes[1].callsign == "CALLSIGN4"
